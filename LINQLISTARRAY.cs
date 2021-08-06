f893a194ef63ca92de1d5588306a2791c8647113
Park seng chul <ochestra365@naver.com>
2021-08-06 09:26:19 +0900

    check

---
+++ LINQLISTARRAY.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace GIT에_올릴_업무코드
+{
+    class LINQLISTARRAY
+    {
+        static void Main(string[] args)
+        {
+            List<int> lstData = new List<int> { 123, 456, 132, 96, 13, 465, 321 };
+            Print("Data : ", lstData);
+            List<int> lstOdd = new List<int>();
+            lstOdd = SelectOddAndSort(lstData);
+            int[] arrEven;
+            //arrEven = SelectOddAndSort(lstData);
+
+        }
+
+        private static List<int> SelectOddAndSort(List<int> lstData)
+        {
+            throw new NotImplementedException();
+        }
+
+        /* private static List<int> SelectOddAndSort(List<int> lstData)
+        {
+            throw 
+            return (from item in lstData
+                    where item % 2 == 0
+                    orderby item
+                    select item).ToArray<int>();
+        }
+        */
+        private static void Print(string s, IEnumerable<int>data)
+        {
+            Console.WriteLine(s);
+            foreach (var i in data) Console.Write(" " + i);
+            Console.WriteLine();
+        }
+    }
+}
